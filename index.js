6a72697a7cad0f1cee17fa9d0022e594cb509e9a
Peter Murphy <peterfrancismurphy@gmail.com>
2018-03-25 19:11:25 -0400

    Basic version of map, no enter, update, exit pattern yet

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-document
-  .getElementById("root")
-  .appendChild(document.createElement("h1"))
+import { selectAll } from "d3-selection"
+import { geoMercator, geoPath } from "d3-geo"
+import data from "./knollwood-data/output/safe-output"
+import massMap from "./json/ma"
 
-document.querySelector("h1").innerText = "HELLO"
+const width = 920
+const height = 920
+
+const svg = selectAll("svg")
+  .attr("width", width)
+  .attr("height", height)
+
+const projection = geoMercator().fitSize(
+  [width, height],
+  massMap
+)
+
+const path = geoPath().projection(projection)
+
+svg
+  .append("path")
+  .attr("d", path(massMap))
+  .style("fill", "red")
+  .style("opacity", "0.2")
+
+svg.append("path").attr("d", path(data))
